dc72f50891bee25cb782b0c3d841988afd7e9eb1
ianmacdonald <ianpetercreative@gmail.com>
2024-01-03 16:46:36 -0700

    add home page route. Adjust package.json file to remove unnecessary dependencies. Update name in package files to portfolio

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,7 @@
 import './NavBar.css'
 import { Link } from 'react-router-dom'
-import * as userService from '../../utilities/users-service'
 
-export default function NavBar({ user, setUser }) {
-    function handleLogOut(){
-        userService.logOut();
-        setUser(null);
-    }
+export default function NavBar() {
     return (
         <nav>
             <div>
