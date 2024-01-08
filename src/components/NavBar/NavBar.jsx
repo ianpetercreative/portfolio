a55672c15d8c847d3bb3e422c40ed4cefe14d78a
ianmacdonald <ianpetercreative@gmail.com>
2024-01-08 10:36:06 -0700

    adjust nav link names. add css for nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ export default function NavBar() {
             <Navbar key="sm" expand="sm" className="mb-3">
                 <Container fluid>
                     <Link to="/">
-                        <Navbar.Brand>Navbar Offcanvas</Navbar.Brand>
+                        <Navbar.Brand className="navbar-brand">ianpetercreative</Navbar.Brand>
                     </Link>
                     <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
                     <Navbar.Offcanvas
@@ -24,11 +24,10 @@ export default function NavBar() {
                             </Offcanvas.Title>
                         </Offcanvas.Header>
                         <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Link to="/" className="nav-link">Home</Link>
+                            <Nav className="justify-content-end flex-grow-1">
                                 <Link to="/about" className="nav-link">About</Link>
-                                <Link to="/work" className="nav-link">Work</Link>
                                 <Link to="/contact" className="nav-link">Contact</Link>
+                                <Link to="/work" className="nav-link btn btn-black">View Work</Link>
                             </Nav>
                         </Offcanvas.Body>
                     </Navbar.Offcanvas>
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,6 @@
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { Route, Routes } from "react-router-dom"
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import NavBar from '../../components/NavBar/NavBar';
 import HomePage from '../HomePage/HomePage';
 import AboutPage from '../AboutPage/AboutPage'
