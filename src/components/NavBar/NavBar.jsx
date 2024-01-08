cdc88168c73e39c1d771fca439a3bbba69a0b11b
ianmacdonald <ianpetercreative@gmail.com>
2024-01-08 11:52:52 -0700

    add home page hero content. move nav css to navbar.css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from 'react-router-dom';
+import './NavBar.css'
 
 export default function NavBar() {
     return (
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+
 export default function HomePage() {
     return (
         <>
-            <h1>HomePage</h1>
+            <div className="grid">
+                <h1 className="home-hero">Software Engineer with a passion for serving others and solving complex problems</h1>
+                <Link to="/work" className="nav-link btn btn-black-2">View Work</Link>
+            </div>
         </>
     );
 }
