50e7fd6cd8975df20ece3efd418326ff721e4d2e
ianmacdonald <ianpetercreative@gmail.com>
2024-01-12 12:35:17 -0700

    add grid section for work on the home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,27 @@
 import { Link } from 'react-router-dom'
+import './HomePage.css'
 
 export default function HomePage() {
     return (
         <>
-            <div className="grid">
-                <h1 className="home-hero">Software Engineer with a passion for serving others and solving complex problems</h1>
-                <Link to="/work" className="nav-link btn btn-black-2">View Work</Link>
-            </div>
+            <section className="home-hero-wrap">
+                <div className="container">
+                    <h1>Software Engineer with a passion for serving others and solving complex problems</h1>
+                    <Link to="/work" className="btn btn-black-jumbo">View Work</Link>
+                </div>
+            </section>
+            <section className="container work-grid">
+                <Link to="/" className="card work-card">
+                    <div>
+                        <h2>reHomr</h2>
+                        <h3>A custom built pet adoption platform</h3>
+                        <img src="images/rehomr/web-screenshots/home.png" alt="rehomr landing page" />
+                    </div>
+                </Link>
+                <Link to="/"></Link>
+                <Link to="/"></Link>
+                <Link to="/"></Link>
+            </section>
         </>
     );
 }
