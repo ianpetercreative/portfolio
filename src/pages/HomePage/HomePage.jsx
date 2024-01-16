4b63dda0d60b2b1c5dbcd14d01b2c719053f2ab5
ianmacdonald <ianpetercreative@gmail.com>
2024-01-16 11:47:53 -0700

    add content and styling to the home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,15 +12,33 @@ export default function HomePage() {
             </section>
             <section className="container work-grid">
                 <Link to="/" className="card work-card">
-                    <div>
+                    <div className="work-card-gradient">
                         <h2>reHomr</h2>
                         <h3>A custom built pet adoption platform</h3>
                         <img src="images/rehomr/xan-griffin-UCFgM_AojFg-unsplash.jpg" alt="rehomr landing page" />
                     </div>
                 </Link>
-                <Link to="/"></Link>
-                <Link to="/"></Link>
-                <Link to="/"></Link>
+                <Link to="/" className="card work-card">
+                    <div className="work-card-gradient">
+                        <h2>reHomr</h2>
+                        <h3>A custom built pet adoption platform</h3>
+                        <img src="images/rehomr/xan-griffin-UCFgM_AojFg-unsplash.jpg" alt="rehomr landing page" />
+                    </div>
+                </Link>
+                <Link to="/" className="card work-card">
+                    <div className="work-card-gradient">
+                        <h2>reHomr</h2>
+                        <h3>A custom built pet adoption platform</h3>
+                        <img src="images/rehomr/xan-griffin-UCFgM_AojFg-unsplash.jpg" alt="rehomr landing page" />
+                    </div>
+                </Link>
+                <Link to="/" className="card work-card">
+                    <div className="work-card-gradient">
+                        <h2>reHomr</h2>
+                        <h3>A custom built pet adoption platform</h3>
+                        <img src="images/rehomr/xan-griffin-UCFgM_AojFg-unsplash.jpg" alt="rehomr landing page" />
+                    </div>
+                </Link>
             </section>
         </>
     );
