a429b3c297c83a915242a32f6efd327bcd99b0d7
ianmacdonald <ianpetercreative@gmail.com>
2024-01-22 15:26:53 -0700

    install dotenv

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+require('dotenv').config(); 
 const nodemailer = require('nodemailer')
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
