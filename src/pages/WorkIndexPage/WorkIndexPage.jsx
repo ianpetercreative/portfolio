3eb3b1439450ca3572a12e81d8431b5f6e25022f
ianmacdonald <ianpetercreative@gmail.com>
2024-01-23 15:19:02 -0700

    add content to work page and style

--- src/pages/WorkIndexPage/WorkIndexPage.jsx
+++ src/pages/WorkIndexPage/WorkIndexPage.jsx
@@ -1,7 +1,65 @@
+import { Link } from 'react-router-dom'
+import { Container, Card } from 'react-bootstrap';
+import './WorkIndexPage.css'
+
 export default function WorkIndexPage() {
     return (
         <>
-            <h1>WorkIndexPage</h1>
-        </>
+            <div className="projects-wrap">
+                <h1>Projects</h1>
+                <span>(sorted alphabetically)</span>
+                <Container>
+                    <Link to="/work/checkers">
+                        <Card>
+                            <Card.Img variant="top" src="images/checkers/starting-position.png" />
+                            <Card.Body>
+                                <Card.Title>
+                                    Checkers
+                                </Card.Title>
+                                <Card.Text>
+                                    A digital rendition of the classic two-player strategy board game.                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Link>
+                    <Link to="/work/rehomr">
+                        <Card>
+                            <Card.Img variant="top" src="images/rehomr/web-screenshots/home.png" />
+                            <Card.Body>
+                                <Card.Title>
+                                    reHomr
+                                </Card.Title>
+                                <Card.Text>
+                                    A web application designed to streamline the pet adoption process.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Link>
+                    <Link to="/work/simmr">
+                        <Card>
+                            <Card.Img variant="top" src="images/simmr/landing-page.png" />
+                            <Card.Body>
+                                <Card.Title>
+                                    Simmr
+                                </Card.Title>
+                                <Card.Text>
+                                    A web application for sharing and reviewing your favorite recipes.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Link>
+                    <Link to="/work/trailhead">
+                        <Card>
+                            <Card.Img variant="top" src="images/trailhead/show-1.png" />
+                            <Card.Body>
+                                <Card.Title>
+                                    The Trailhead
+                                </Card.Title>
+                                <Card.Text>
+                                    A web application that helps outdoor enthusiasts discover and share their favorite hiking trails.                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Link>
+                </Container>
+            </div>        </>
     );
 }
